0d86aea5295dc06805dceda7250f256b30ba2e23
hemanthdatta <kothurihemanthdatta@gmail.com>
2025-10-23 23:06:14 +0530

    Added import stmts and sample datset

---
+++ Day2/Day2_Task.py
@@ -0,0 +1,13 @@
+#Day-2
+
+Preprocessing (Commit 1)
+
+# Import pandas
+import pandas as pd
+
+# Create a simple dataset
+data = {'Name': ['Alice', 'Bob', 'Charlie'], 'Age': [25, 30, 35]}
+df = pd.DataFrame(data)
+
+# Display the dataset
+print(df)
